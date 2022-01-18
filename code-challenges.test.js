e3a434dc796826781666a6fd1cec7e8c4288b679
kickeyhickey <irishmanhickey@gmail.com>
2022-01-18 08:54:54 -0800

    finished

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -23,9 +23,31 @@ const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
 
+describe("hello", () => {
+  it("takes in a string and converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0 ", () => {
+    const secretCodeWord1 = "Lackadaisical"
+    const secretCodeWord2 = "Gobbledygook"
+    const secretCodeWord3 = "Eccentric"
+    //use expect to take in variables and return expected output with .toEqual
+    expect(codeMaker(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+    expect(codeMaker(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+    expect(codeMaker(secretCodeWord3)).toEqual("3cc3ntr1c")
+  })
+})
+
+    // ERROR: codeMaker is not defined
 
 // b) Create the function that makes the test pass.
 
+// create a function that takes in a string. 
+const codeMaker = (string) =>{
+  // define letters to replace and iterate over string replacing those values
+  // use .replace and create a regex. Use built in replace funtion by creating an object with key value pairs
+  return string.replace(/[aeioAEIO]/g, value => ({'a': 4, 'A':4, 'e': 3, 'E': 3, 'i': 1, 'I': 1, 'o': 0, 'O':0}) [value])
+}
+console.log(codeMaker(secretCodeWord1));
+console.log(codeMaker(secretCodeWord2));
+console.log(codeMaker(secretCodeWord3));
 
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns all the words that contain that particular letter.
@@ -39,23 +61,78 @@ const arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
 const letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
-
-
+describe("onlyWords", () => {
+  it("takes in an array of words and a single letter and returns all the words that contain that particular letter.", () => {
+    const arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
+    const letterA = "a"
+    const arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
+    const letterE = "e"
+    //use expect to take in variables and return expected output with .toEqual
+    expect(onlyWords(arrayOfWords1, letterA)).toEqual(["Apple", "Banana", "Orange"])
+    expect(onlyWords(arrayOfWords2, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+  })
+})
+    //ERROR: onlyWords is not defined
 // b) Create the function that makes the test pass.
 
-
+// create a function that takes in an array and a single letter
+const onlyWords = (words, letter) =>{
+  //iterate over the array and test if each value has the letter argument
+  // find a use for .toUpperCase()
+  return words.filter(value => value.includes(letter) || value.includes( letter.toUpperCase()) ? value : null)
+}
+console.log(onlyWords(arrayOfWords1, letterA));
+console.log(onlyWords(arrayOfWords2, letterE));
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
+describe("poker", () => {
+  it("a function that takes in an array of 5 numbers and determines whether or not the array contains a pair and three of a kind", () => {
+    const hand1 = [5, 5, 5, 3, 3]
+    // Expected output: true
+    const hand2 = [5, 5, 3, 3, 4]
+    // Expected output: false
+    const hand3 = [5, 5, 5, 5, 4]
+    // Expected output: false
+    //use expect to take in variables and return expected output with .toEqual
+    expect(poker(hand1)).toEqual(true)
+    expect(poker(hand2)).toEqual(false)
+    expect(poker(hand3)).toEqual(false)
+  })
+})
 
+    // ERROR: poker is not defined
 
 
 // b) Create the function that makes the test pass.
+
+
+
+
+
+    const hand1 = [5, 5, 5, 3, 3]
+    // Expected output: true
+    const hand2 = [5, 5, 3, 3, 4]
+    // Expected output: false
+    const hand3 = [5, 5, 5, 5, 4]
+    // Expected output: false   
+
+
+    // create function that takes in an array. Define a variable to catch the the number of times a value is repeated in the array
+const poker = (array) =>{
+    let nums = []
+    // iterate over array and push the sum of the repeated numbers to the nums array
+    array.map((v,i,a) =>{
+      //create a variable to hold our sum by setting it equal to each value in the array
+      let newVal = a[i]
+      // ask if our empty array already contains a value. If yes add 1 to an index, if no the value only recieves a 1 count
+      nums[newVal] ? nums[newVal] += 1 : nums[newVal] = 1
+    })
+    // Now we look into our array to see if a value was counted 3 times and 2 times
+    return nums.includes(3) && nums.includes(2) ? true : false
+}
+console.log(poker(hand1));
+console.log(poker(hand2));
+console.log(poker(hand3));
--- interview-questions.md
+++ interview-questions.md
@@ -7,53 +7,53 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is a block in Ruby?
 
-  Your answer:
+  Your answer: A block in Ruby is an anonymous function that can have arguments.
 
-  Researched answer:
+  Researched answer: An anonymous function that can be passed into methods. They are enclosed in curly braces. They can contain arguments and can be single line
 
 
 
 2. What is a gem?
 
-  Your answer:
+  Your answer: A gem is a package manager that allows you to download additional software for Ruby that helps with projects. 
 
-  Researched answer:
+  Researched answer: A gem is a packaged library that can be installed to support the Ruby programming language.
 
 
 
 3. What is Ruby on Rails?
 
-  Your answer:
+  Your answer:  An open sourced software that uses the Ruby programming language. It's used for web page development and for the creation of databases
 
-  Researched answer:
+  Researched answer: Ruby on Rails is a full stack framework that uses the Ruby programming language to build web apps on the front and back ends of development. 
 
 
 
 4. What is a relational database? Are there other kinds of databases?
 
-  Your answer:
+  Your answer: Relational databases store data in tables that are called with keys. 
 
-  Researched answer:
+  Researched answer: Like an excel spreadsheet they use rows and columns to store data. Some other types of databases include NoSql (NoSql databases don't deploy the use of a schema), columnar (store data in columns rather than rows), and wide column databases ( data is stored in column families that can handle massive amounts of values. Ideal for big data apps)
 
 
 
 5. What are primary keys? Why are they important?
 
-  Your answer:
+  Your answer: Primary keys are values, typically numbers, given to rows are columns of data. These keys are assigned by the program and can't be changed. This is essential in finding paticular sets of data in a database. 
 
-  Researched answer:
+  Researched answer: A uniquely assigned identifier contained in a column on your table that calls a certain row of data. 
 
 
 
 ## Looking Ahead: Terms for Next Week
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: A naming pattern that connects HTTP verbs and Crud actioans. It listens and recieves requests then decides what to send back to the user. 
 
-2. JSON:
+2. JSON: javascrilpt object notation is a text based format. It closely resembles JS object notation and is used to translate data between servers and the end user.
 
-3. ERB:
+3. ERB: A system that embeds Ruby into a plain text document or HTML file.
 
-4. Params:
+4. Params: a parameters method in Ruby on Rails is a hash that is a collection of data for certain HTTP requests.
 
-5. API:
+5. API: Application programming interface. A communication between two softwares. A messenger that takes a user interaction, delivers the data to a server, and then returns the results. 
